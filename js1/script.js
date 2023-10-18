ef7cde8c8e7646e2c4e1964585ae38441eecf652
shemchuk <shemchukaga@gmail.com>
2023-10-18 15:18:16 +0200

    работа на занятии 18ю10

---
+++ js1/script.js
@@ -0,0 +1,39 @@
+//alert("Hello")
+/*
+
+*/
+// console.log(100)
+// let name = "Woshe";
+// console.log(name);
+// name = "Arsen";
+// console.log(name);
+
+// const userName = ("Woshe");
+// console.log(userName);
+//userName = "Arsen"
+//console.log(userName)
+
+// const number = prompt("Введите число") 
+// console.log(number)
+// console.log(typeof number)
+
+// const number1 = +prompt("Введите число") 
+// console.log(number1)
+// console.log(typeof number1)
+
+
+
+// const userName = prompt("Введите имя")
+// console.log(` ${userName}`)
+
+// const r =100
+// const g = 57
+// const b = 141
+
+// console.log(`rgb(${r}, ${g}, ${b})`)
+
+// const age = +prompt("Введите Ваш год рождения")
+
+// console.log(`Вам ${2023 - age} лет`)
+
+
